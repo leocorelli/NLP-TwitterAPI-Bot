31619ebfb0de9709b50bd92cbd1b6fb15b2e804d
lcorelli <leonardcorelli@gmail.com>
2022-01-30 23:40:38 -0500

    made big changes

--- main.py
+++ main.py
@@ -1,96 +1,32 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-
-templates = Jinja2Templates(directory='htmldirectory')
-
-
 import uvicorn
-import requests
+from searchtweets import gen_request_parameters, load_credentials, collect_results 
 import pandas as pd
-import boto3
-from decouple import config
-
 from transformers import pipeline
 import numpy as np
 import time
+from decouple import config
+import os
 
-bearer_token = config('BEARER_TOKEN') # coming from .env file (it's an environment variable)
-access_key = config('AWS_ACCESS_KEY_ID')
-secret_key = config('AWS_SECRET_ACCESS_KEY')
+os.environ["SEARCHTWEETS_BEARER_TOKEN"] = config('SEARCHTWEETS_BEARER_TOKEN') 
+os.environ["SEARCHTWEETS_CONSUMER_KEY"] = config('SEARCHTWEETS_CONSUMER_KEY')
+os.environ["SEARCHTWEETS_CONSUMER_SECRET"] = config('SEARCHTWEETS_CONSUMER_SECRET')
+os.environ["SEARCHTWEETS_ENDPOINT"] = config('SEARCHTWEETS_ENDPOINT')
 
-def initialize_parameters(term, max_results = 10, bearer_token = bearer_token):
-    search = f'{term} -is:retweet'
-    query = f'https://api.twitter.com/2/tweets/search/recent?query={search}&max_results={max_results}'
-    my_headers = {'Authorization' : 'Bearer ' + bearer_token}
-    print(f'\nSearch term: {term}')
-    return query, my_headers
+templates = Jinja2Templates(directory='htmldirectory')
 
-
-def call_api(query, my_headers, times = 1):
-    unclean_data = []
-    for i in range(times):
-        if i == 0:
-            response = requests.get(query, headers = my_headers)
-            response = response.json()
-            data = response['data']
-            meta = response['meta']
-            next_token = meta['next_token']
-            
-            unclean_data.extend(data)
-
-        else:
-            new_query = query + '&next_token=' + next_token
-            
-            response = requests.get(new_query, headers = my_headers)
-            response = response.json()
-            data = response['data']
-            meta = response['meta']
-            next_token = meta['next_token']
-            
-            unclean_data.extend(data)
-    
-    return unclean_data
-
-
-def convert_to_list(messy_data):
-    '''Turns messy json repsonse into a list of extracted tweets
-    
-    Args:
-        messy_data (list of dicts extracted from json response): 'data' part of .json() parse
-        
-    Returns:
-        list: the cleaned list that has the extracted tweets as elements
-    '''
-    clean_list = []
-    
-    for i in range(len(messy_data)):
-        clean_list.append(messy_data[i]['text'])
-    return clean_list
-
-def convert_to_df(lst_of_tweets):
-    df = pd.DataFrame(lst_of_tweets, columns = ['tweet'])
-    for i in range(len(df)):
-        df['tweet'].iloc[i] = df['tweet'].iloc[i].split(' https')[0]
-
+def get_tweets(term, max_tweets = 200):
+    v2_search_args = load_credentials()
+    query = gen_request_parameters(f"{term} -is:retweet -is:reply", results_per_call=100, granularity = None) 
+    tweets = collect_results(query,result_stream_args=v2_search_args, max_tweets = max_tweets)
+    lst = []
+    for i in range(len(tweets)):
+        for j in range(len(tweets[i]['data'])):
+            lst.append(tweets[i]['data'][j]['text'])
+    df = pd.DataFrame(lst,columns = ['tweet'])
     df = df.drop_duplicates(ignore_index = True)
     return df
-
-def detect_sentiment(tweets):
-    comprehend = boto3.client(service_name='comprehend', region_name='us-east-1', aws_access_key_id = access_key, aws_secret_access_key = secret_key)
-    vals = {'POSITIVE': 0, 'NEUTRAL': 0, 'NEGATIVE': 0, 'MIXED': 0}
-    
-    print('Calling DetectSentiment')
-    print(f'Analyzing {len(tweets)} total tweets\n')
-
-    for i in range(len(tweets)):
-        if i%50 == 0:
-            print(f'{i}/{len(tweets)}')
-        sentiment = comprehend.detect_sentiment(Text=tweets['tweet'].iloc[i], LanguageCode='en')
-        vals[sentiment['Sentiment']] += 1
-    
-    print('\nEnd of DetectSentiment\n')
-
-    return vals
 
 def detect_sentiment_new(tweets):
     start = time.time()
@@ -136,14 +72,12 @@ async def root(request: Request):
 @app.get("/RealNLP")
 async def realNLP(request: Request, term: str):
     """New and improved NLP sentiment analysis"""
-
-    query, my_headers = initialize_parameters(term, max_results = 100)
-    unclean_data = call_api(query, my_headers, times = 2)
-    tweets = convert_to_list(unclean_data)
-    tweets = convert_to_df(tweets)
-    positive, negative, total = detect_sentiment_new(tweets)
-    #return {"Positive": positive, "Negative": negative, "Total # of tweets not classified": not_classified, "Total # of tweets": total}
-    return templates.TemplateResponse("away.html", {"request": request, "term":term, "positive":positive, "negative":negative, "total":total})
+    try:
+        tweets = get_tweets(term)
+        positive, negative, total = detect_sentiment_new(tweets)
+        return templates.TemplateResponse("away.html", {"request": request, "term":term, "positive":positive, "negative":negative, "total":total})
+    except:
+        return templates.TemplateResponse("third.html", {"request": request, "term":term})
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')
